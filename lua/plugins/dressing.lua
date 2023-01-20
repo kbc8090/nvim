0e22de0d9f535cde84c9e1a115f9a893c7e31f82
zerix <zerix@zerix.net>
2023-01-19 20:13:33 -0500

    new nvim config in lua

---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,17 @@
+return {
+	'stevearc/dressing.nvim',
+	cond = false,
+	config = function() 
+		require('dressing').setup({
+			input = {
+				enabled = true,
+				default_prompt = "➤ ",
+				win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" },
+			},
+			select = {
+				backend = { "telescope" },
+				builtin = { win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" } },
+			},
+		}) 
+	end
+}
