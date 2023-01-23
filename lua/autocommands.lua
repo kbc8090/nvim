50adb100f3f9a701d7f3b2fba7854a79c5e69c11
zerix <zerix@zerix.net>
2023-01-23 00:06:29 -0500

    vims to lua

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -3,7 +3,7 @@ local cursorline_fg = vim.api.nvim_get_hl_by_name("Constant", true)
 vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = cursorline_fg.foreground, bold = true })
 
 -- Set cursor back to Beam when exiting Neovim
-vim.api.nvim_create_autocmd("ExitPre", {
+vim.api.nvim_create_autocmd({ "ExitPre" }, {
   pattern = "*",
   callback = function()
     vim.cmd.set('guicursor=a:ver20')
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd("ExitPre", {
 })
 
 -- Spawn help window in vertical split
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = "help",
   callback = function()
     vim.cmd.wincmd('L')
@@ -19,7 +19,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	callback = function()
 		vim.highlight.on_yank()
 	end,
@@ -28,12 +28,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
 	callback = function()
-		vim.cmd("tabdo wincmd =")
+		vim.cmd.tabdo('wincmd =')
 	end,
 })
 
 -- go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
 	callback = function()
 		local mark = vim.api.nvim_buf_get_mark(0, '"')
 		local lcount = vim.api.nvim_buf_line_count(0)
