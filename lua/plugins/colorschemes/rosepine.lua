ce7f7a2434337ccdea4df124d177ed89b26e3993
zerix <zerix@zerix.net>
2023-02-01 21:24:09 -0500

    creating rainbowcols for rosepine

--- lua/plugins/colorschemes/rosepine.lua
+++ lua/plugins/colorschemes/rosepine.lua
@@ -4,6 +4,39 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("rose-pine").setup()
+    require("rose-pine").setup({
+      dark_variant = 'moon',
+      disable_background = true,
+      highlight_groups = {
+        rainbowcol1 = {
+          fg = 'rose',
+          bold = true,
+        },
+        rainbowcol2 = {
+          fg = 'pine',
+          bold = true,
+        },
+        rainbowcol3 = {
+          fg = 'iris',
+          bold = true,
+        },
+        rainbowcol4 = {
+          fg = 'gold',
+          bold = true,
+        },
+        rainbowcol5 = {
+          fg = 'love',
+          bold = true,
+        },
+        rainbowcol6 = {
+          fg = 'love',
+          bold = true,
+        },
+        rainbowcol7 = {
+          fg = 'foam',
+          bold = true,
+        },
+      }
+    })
   end
 }
