38354b0b17599b3358b3757abdcfe51bbe05a20c
zerix <zerix@zerix.net>
2023-04-27 08:47:29 -0400

    gruvbox italic change

--- lua/plugins/colorschemes/gruvbox.lua
+++ lua/plugins/colorschemes/gruvbox.lua
@@ -8,7 +8,7 @@ return {
       -- undercurl = true,
       -- underline = true,
       bold = true,
-      italic = true,
+      -- italic = true,
       -- strikethrough = true,
       -- invert_selection = false,
       -- invert_signs = false,
