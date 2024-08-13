8ca86dc087524d69905956f1d56a65c5d9aff3ee
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2024-08-13 00:29:44 -0400

    Add files via upload

--- lua/plugins/colorschemes/kanagawa.lua
+++ lua/plugins/colorschemes/kanagawa.lua
@@ -12,26 +12,50 @@ return {
       transparent = true,         -- do not set background color
       dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
       terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none"
+            }
+          }
+        }
+      },
       overrides = function(colors) -- add/modify highlights
+        local default = colors.palette
         local theme = colors.theme
         return {
-          LazyNormal = { bg = "none", fg = "red"},
           NormalFloat = { bg = "none" },
           FloatBorder = { bg = "none" },
           FloatTitle = { bg = "none" },
           NoiceCmdlineIcon = { bg = "none", fg = theme.ui.fg_dim },
           NoiceCmdlinePopupBorder = { bg = "none", fg = theme.ui.special },
-          RainbowDelimiterBlue = { bold = true },
-          RainbowDelimiterCyan = { bold = true },
-          RainbowDelimiterGreen = { bold = true },
-          RainbowDelimiterOrange = { bold = true },
-          RainbowDelimiterRed = { fg = theme.syn.number, bold = true },
-          RainbowDelimiterViolet = { bold = true },
-          RainbowDelimiterYellow = { fg = theme.syn.string, bold = true },
+          -- depth 1
+          RainbowDelimiterRed = { fg = default.sakuraPink, bold = true },
+          -- depth 2
+          RainbowDelimiterYellow = { fg = default.carpYellow, bold = true },
+          -- depth 3
+          RainbowDelimiterBlue = { fg = default.surimiOrange, bold = true },
+          -- depth 4
+          RainbowDelimiterGreen = { fg = default.springGreen, bold = true },
+          -- depth 5
+          RainbowDelimiterCyan = { fg = default.waveAqua2, bold = true },
+          -- depth 6
+          RainbowDelimiterOrange = { fg = default.springViolet1, bold = true },
+          -- depth 7
+          RainbowDelimiterViolet = { fg = default.sakuraPink, bold = true },
+--          RainbowDelimiterBlue = { bold = true },
+--          RainbowDelimiterCyan = { bold = true },
+--          RainbowDelimiterGreen = { bold = true },
+--          RainbowDelimiterOrange = { bold = true },
+--          RainbowDelimiterRed = { fg = theme.syn.number, bold = true },
+--          RainbowDelimiterViolet = { bold = true },
+--          RainbowDelimiterYellow = { fg = theme.syn.string, bold = true },
           TelescopeTitle = { fg = theme.syn.number, bold = true },
           TelescopePromptPrefix = { fg = theme.syn.string },
           TelescopeSelection = { bg = theme.ui.bg_search },
           Operator = { fg = theme.syn.number },
+          BufferLineIndicatorSelected = { fg = default.sakuraPink },
           -- Save an hlgroup with dark background and dimmed foreground
           -- so that you can use it where your still want darker windows.
           -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
@@ -39,7 +63,7 @@ return {
 
           -- Popular plugins that open floats will link to NormalFloat by default;
           -- set their background accordingly if you wish to keep them dark and borderless
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          LazyNormal = { bg = "none", fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
         }
       end,
